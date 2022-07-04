b102874402de6a1ef47c6e138deadb957685dd9a
smjsih@naver.com <smjsih@naver.com>
2022-07-04 15:11:04 +0900

    Initial commit

---
+++ src/main/java/com/example/blog/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.blog.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
